d16e2f5d6c810b8cb66e360ddbf2f812bee274d5
Naphal-AXT <diegocampal@icloud.com>
2026-07-14 21:39:20 +0200

    Add initial correlation engine

---
+++ src/Plugins/ThePiper.Housing/Correlation/HousingCorrelationEngine.cs
@@ -0,0 +1,11 @@
+using ThePiper.Housing.Core;
+
+namespace ThePiper.Housing.Correlation;
+
+public sealed class HousingCorrelationEngine
+{
+    public HousingEntry Correlate(HousingEntry entry)
+    {
+        return entry;
+    }
+}
