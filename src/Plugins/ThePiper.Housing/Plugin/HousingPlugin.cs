c5e22388fe86bcee62546b546fcd4a0a6ef8f383
Naphal-AXT <diegocampal@icloud.com>
2026-07-14 21:39:50 +0200

    Add initial HousingPlugin entry point

---
+++ src/Plugins/ThePiper.Housing/Plugin/HousingPlugin.cs
@@ -0,0 +1,11 @@
+namespace ThePiper.Housing.Plugin;
+
+public sealed class HousingPlugin
+{
+    public string Name => "ThePiper Housing";
+    public string Version => "0.1.0";
+
+    public void Initialize()
+    {
+    }
+}
