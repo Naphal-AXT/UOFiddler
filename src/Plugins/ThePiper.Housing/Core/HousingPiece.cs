c02bbfd9784b5483ecfedf36befd49c43f15b96f
Naphal-AXT <diegocampal@icloud.com>
2026-07-14 21:33:38 +0200

    Add HousingPiece model

---
+++ src/Plugins/ThePiper.Housing/Core/HousingPiece.cs
@@ -0,0 +1,9 @@
+namespace ThePiper.Housing.Core;
+
+public sealed class HousingPiece
+{
+    public ushort TileId { get; set; }
+    public short X { get; set; }
+    public short Y { get; set; }
+    public sbyte Z { get; set; }
+}
