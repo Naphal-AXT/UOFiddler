27c030a9fc031eacededc2a649b11b77ff700e1f
Naphal-AXT <diegocampal@icloud.com>
2026-07-14 21:44:09 +0200

    Implement TXT definition loading skeleton

--- src/Plugins/ThePiper.Housing/Import/TxtHousingDefinitionSource.cs
+++ src/Plugins/ThePiper.Housing/Import/TxtHousingDefinitionSource.cs
@@ -1,12 +1,25 @@
 using System.Collections.Generic;
+using System.IO;
 using ThePiper.Housing.Core;
 
 namespace ThePiper.Housing.Import;
 
 public sealed class TxtHousingDefinitionSource : IHousingDefinitionSource
 {
+    public string? RootPath { get; init; }
+
     public IEnumerable<TileReference> Load()
     {
-        yield break;
+        if (string.IsNullOrWhiteSpace(RootPath) || !Directory.Exists(RootPath))
+            yield break;
+
+        foreach (var file in Directory.EnumerateFiles(RootPath, "*.txt"))
+        {
+            foreach (var line in File.ReadLines(file))
+            {
+                if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#"))
+                    continue;
+            }
+        }
     }
 }
